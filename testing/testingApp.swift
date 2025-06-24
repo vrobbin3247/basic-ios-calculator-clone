398dce037bc4c7db4ff6a60f90a40c273cc8e586
vrobbin3247 <vaibhavgame3247@gmail.com>
2025-06-24 14:07:15 +0530

    Initial Commit

---
+++ testing/testingApp.swift
@@ -0,0 +1,17 @@
+//
+//  testingApp.swift
+//  testing
+//
+//  Created by Vaibhav Mandavkar on 24/06/25.
+//
+
+import SwiftUI
+
+@main
+struct testingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ testingTests/testingTests.swift
@@ -0,0 +1,17 @@
+//
+//  testingTests.swift
+//  testingTests
+//
+//  Created by Vaibhav Mandavkar on 24/06/25.
+//
+
+import Testing
+@testable import testing
+
+struct testingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
